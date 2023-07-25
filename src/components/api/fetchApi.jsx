04f1b5aef388162ba3413befc41643b91ba9c0ef
Izabela Świadkowska <izaswiadkowska@gmail.com>
2023-07-25 19:26:09 +0200

    correct added

--- src/components/api/fetchApi.jsx
+++ src/components/api/fetchApi.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 axios.defaults.params = {
-  key: process.env.REACT_APP_API_KEY,
+  key: '36775906-ed808f9769460bb0dab61703f',
   image_type: 'photo',
   orientation: 'horizontal',
   per_page: 12,
